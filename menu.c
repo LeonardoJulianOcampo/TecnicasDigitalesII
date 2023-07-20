53b7b5d3d1ea2749638c2d499c8b9ad2c3f6a0d6
Leonardo Ocampo <leonardoj.ocampo@gmail.com>
2023-07-20 06:53:04 -0300

    modifica la funcion menu. corrige funcionamiento erroneo al querer salir de una funcion que maneja leds

--- menu.c
+++ menu.c
@@ -14,7 +14,7 @@ int menu(void){
 	
 	op=1;
 	
-	while(op!=0){
+	while(op != -1){
 
 	system("clear");
 	printf("\n***MENU***");	
@@ -22,24 +22,35 @@ int menu(void){
 	printf("\n1)Apilada");
 	printf("\n2)Auto Fantastico");
 	printf("\n3)Choque");
-	printf("\n4)Salir");
+	printf("\n4)Salir\n");
 	scanf("%d",&op);
 	
 	 switch (op)
 	 {
-	 case 1: autofan();
+	 case 1: apilada();
 	 	op = 0;
 		 break;
-	 case 2: apilada();
+	 case 2: autofan();
 	 	op = 0;
 	 	 break;	
 	 case 3: choque();
 	 	op=0;
 		break;
-	 default: printf("\nOperacion Invalida");
-		 op=0;
+
+	 case 4: 
+		settings();
+	 	op = 0;
+		break;
+	
+	case 5: 
+		op = -1;
+
+	default: printf("\nOperacion Invalida");
 		 break;
 	 }
 		
+	 if (op == -1)
+		 break;
+
 	}
 }
