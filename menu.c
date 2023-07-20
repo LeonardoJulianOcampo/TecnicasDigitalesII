d294e2519c99d11d84fbfd3565abb5a763b7b33e
Leonardo Ocampo <leonardoj.ocampo@gmail.com>
2023-07-20 19:14:03 -0300

    Modifica la funcion contrasenia. Ahora emplea la libreria ncurses para el ingreso de la clave y la impresion de pantalla Modifica la funcion menu, para que ahora muestre un menú con opciones seleccionables con el teclado

--- menu.c
+++ menu.c
@@ -1,76 +1,90 @@
 /*no recibe ningun valor. Imprime las opciones disponibles. Cuando se selecciona un valor correcto
- se devuelve ese valor como entero para ser tomado por la funcion 'efecto'
- */
+ se devuelve ese valor como entero para ser tomado por la funcion 'efecto'*/
+
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
 
-#define OPTIONS 4
+#define OPTIONS 3
 
-int counter(int select){
-	static int count = 0;
-	if (select == 1 && count < OPTIONS)
-		count ++;
-	else if (select == 1 && count >= OPTIONS)
-		count = 0;
-	else if (select == 0 && count > 0)
-		count = count--;
-	else if (select == 0 && count == 0)
-		count = OPTIONS - 1;
-	else 
-		count = count;
+int counter(int select) {
+    static int count = 0;
 
+    if (select == 1 && count < OPTIONS - 1)
+        count++;
+    else if (select == 1 && count == OPTIONS - 1)
+        count = 0;
+    else if (select == 0 && count > 0)
+        count--;
+    else if (select == 0 && count == 0)
+        count = OPTIONS - 1;
+    else
+        count = count;
 
-	return count;
+    return count;
+}
+
+void menu(void) {
+    int op, row, col;
+    int exit = 0;
+    char mesg[] = "   MENU PRINCIPAL   ";
+    char options[OPTIONS][30] = {
+        "Selección de Efectos",
+        "      Ajustes       ",
+	"       Salir        ",
+    };
+
+    initscr();
+    getmaxyx(stdscr, row, col);
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+    nodelay(stdscr, FALSE); // Importante para que getch() espere a que se presione una tecla
+
+    while (!exit) {
+        clear();
+        mvprintw(row / 2, (col - strlen(mesg)) / 2, mesg);
+
+        for (int i = 0; i < OPTIONS; i++) {
+            if (i == counter(-1)) {
+                attron(A_STANDOUT); // Resaltar opción seleccionada
+                mvprintw(row / 2 + i + 1, (col - strlen(options[i])) / 2, "-> %s", options[i]);
+                attroff(A_STANDOUT);
+            } else {
+                mvprintw(row / 2 + i + 1, (col - strlen(options[i])) / 2, "   %s", options[i]);
+            }
+        }
+
+        refresh();
+        op = getch();
+
+        switch (op) {
+            case KEY_UP:
+                counter(0);
+                break;
+            case KEY_DOWN:
+                counter(1);
+                break;
+            case 10: // Enter key
+                if (counter(-1) == OPTIONS - 1)
+                    exit = 1;
+                else
+                    mvprintw(row, 0, "Opción seleccionada: %s", options[counter(-1)]);
+                refresh();
+                //getch(); // Esperar a que el usuario presione una tecla para continuar
+                break;
+            case 27: // Escape key
+                exit = 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+    endwin();
+
 }
 
 
-
-int menu(void){
-	
-	int op,row,col;
-	int exit = 0;
-	char mesg[]="MENU PRINCIPAL";
-	char mesg_efecto[]="Primer item";
-
-	initscr();                                 // inicializa el modo curses
-	getmaxyx(stdscr,row,col);                    // obtiene el máximo valor de filas y columnas de la ventana actual
-	raw();                                     
-	keypad(stdscr,TRUE);
-	noecho();
-	nodelay(stdscr,TRUE);
-	
-	while(!exit){
-		
-		mvprintw(row    ,(col-strlen(mesg))/2,"-> %s",mesg);
-		mvprintw(row + 2,(col-strlen(mesg_efecto))/2 + 2,"%s",mesg_efecto);
-		refresh();
-		op = getch();
-
-		switch(op){
-			
-			case KEY_UP: 
-				clear();
-				mvprintw(counter(1)*2,(col-strlen(mesg))/2,"->");
-				refresh();
-				break;
-			case KEY_DOWN:
-				clear();
-				mvprintw(counter(0)*2,(col-strlen(mesg))/2,"->");
-				refresh();
-				break;
-			case KEY_F(2):
-				clear();
-				refresh();
-				exit = 1;
-				break;
-				
-		}
-		
-	}	
-	endwin();
-	
-	return 0;
-
-}
